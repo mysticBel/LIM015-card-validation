182ff9c641b07ded1ba8b33120d35f3f55493966
mysticBel <dnxbel@gmail.com>
2021-07-31 04:32:34 -0500

    added eyeSelected

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-import validator from './validator.js';
+// import validator from './validator.js';
 
-console.log(validator);
+
+/* eye option -show password*/
+let eyeSelected= false;
+const eye = document.getElementById("eye");
+eye.addEventListener("click", function toggle(){
+  if(eyeSelected){
+    document.getElementById("numberInput").setAttribute("type","password");
+    eyeSelected=false;
+  }   
+  else{
+    document.getElementById("numberInput").setAttribute("type","text");
+    eyeSelected=true;
+  }});
+
+// only accept numbers in numberInput
+const inputOnlyNumbers = document.getElementById("numberInput");   
+inputOnlyNumbers.addEventListener("keypress", function onlyNumbers(x){
+  let digitKeycode = inputOnlyNumbers.event ?  x.which :x.keyCode;
+      if (digitKeycode < 48 || digitKeycode > 57) {
+        x.preventDefault();
+       }
+    }); 
+
+
+
+// console.log(validator);
